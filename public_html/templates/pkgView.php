9bb3a9a11a31cde428a217ade1c9b0f545e345ef
Trevor Pelton <tnpflyer20@msn.com>
2017-03-13 17:17:09 -0600

    removing button

--- public_html/templates/pkgView.php
+++ public_html/templates/pkgView.php
@@ -2,14 +2,7 @@
 	<div class="row">
 		<div class="col-xs-12">
 			<h2>Parking</h2>
-			<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">+</button>
-			<div id="myModal" class="modal fade">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<h4 class="modal-title">Confirmation</h4>
-						</div>
+
 			<table class="table table-bordered table-hover table-parking">
 				<thead>
 					<tr>
