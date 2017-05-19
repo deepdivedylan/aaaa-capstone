033f71c5aedcecb27f96851e46d84b17e1378552
Maria Rosado <mrosado2@cnm.edu>
2017-05-19 13:32:33 -0600

    re uploaded images on css file

--- webpack/index.php
+++ webpack/index.php
@@ -7,6 +7,7 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
+		<
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
