61a14a7c07620923476cd8d1bf54528f1483db95
Maria Rosado <mrosado2@cnm.edu>
2017-05-23 15:04:30 -0600

    adding logo

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -4,12 +4,13 @@
 	<div class="navbar-header">
 		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
 				  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<img class="pull-left logo visible-xs" src="images/cnm_Ingenuity_logo.phg" />
 			<span class="sr-only">Toggle navigation</span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand">CNM Stream Line</a>
+		<a class="navbar-brand">CNM Ingenuity, Inc</a>
 	</div>
 
 	<!-- Collect the nav links, forms, and other content for toggling -->
