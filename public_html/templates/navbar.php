0af75bd2c625205075c6898d56aab9157aece409
Matthew Fisher <m.r.f.48215@gmail.com>
2017-05-23 11:06:47 -0600

    Added modal for getting emails by note type

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -26,6 +26,47 @@
 			<li><a routerLink="/appView/">Applications</a></li>
 			<li><a routerLink="/mobView/">Quick Prospect</a></li>
 			<li><a routerLink="/prsView/">Prospects</a></li>
+			<li><!-- Button trigger modal -->
+				<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+					Mailing List
+				</button>
+
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<h4 class="modal-title" id="myModalLabel">Mailing List</h4>
+							</div>
+							<div class="modal-body">
+								<!-- FORM -->
+								<form #mailListForm="ngForm" name="mailList" id="mailList" class="form-horizontal well"
+										(ngSubmit)="createMailList();" novalidate>
+									<div class="modal-body">
+										<label>Note Type:</label>
+										<select class="form-control" id="noteNoteTypeId" name="noteNoteTypeID"
+												  [(ngModel)]="note.noteNoteTypeId" required>
+											<option *ngFor="let noteType of noteTypes" value="{{noteType.noteTypeId}}">
+												{{noteType.noteTypeName}}
+											</option>
+										</select>
+									</div>
+									<div class="modal-footer">
+										<input type="submit" class="btn btn-info" value="Submit">
+										<div class="jumbotron">
+
+										</div>
+									</div>
+								</form>
+								<!-- FORM -->
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							</div>
+						</div>
+					</div>
+				</div></li>
 
 			<li role="separator" class="divider"></li>
 		</ul>
